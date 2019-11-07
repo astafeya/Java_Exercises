1fa77103369066b0cca4dc0ddcec4db8dd677ff7
Astafeva Irina <astafeya99@gmail.com>
2019-11-08 02:46:53 +0300

    add Person, Stsff and Student classes

---
+++ src/Persons/Person.java
@@ -0,0 +1,51 @@
+package Persons;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person() {
+    }
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person[name = " + name + ", address = " + address + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Person)) {
+            return false;
+        }
+        Person person = (Person) obj;
+        return name.equals(person.name) && address.equals(person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        int code = 17;
+        code = 31 * code + name.hashCode();
+        code = 31 * code + address.hashCode();
+        return code;
+    }
+}
---
+++ src/Persons/Staff.java
@@ -0,0 +1,55 @@
+package Persons;
+
+public class Staff extends Person {
+    private String school;
+    private double pay;
+
+    public Staff(String name, String address, String school, double pay) {
+        super(name, address);
+        this.school = school;
+        this.pay = pay;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff[" + super.toString() + ", school = " + school + ", pay = " + pay +"]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Staff)) {
+            return false;
+        }
+        Staff staff = (Staff) obj;
+        return school.equals(staff.school) && pay == staff.pay && getName() == staff.getName() &&
+                getAddress() == staff.getAddress();
+    }
+
+    @Override
+    public int hashCode() {
+        int code = 17;
+        code = 31 * code + super.hashCode();
+        code = 31 * code + school.hashCode();
+        code = 31 * code + (int) pay;
+        return code;
+    }
+}
