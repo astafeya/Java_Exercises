9ce734d88b84f32f69bdcb43b6537a317bceb1d6
Astafeva Irina <astafeya99@gmail.com>
2019-11-11 10:10:38 +0300

    fix ResizableCircle

--- src/ResizableObjects/ResizableCircle.java
+++ src/ResizableObjects/ResizableCircle.java
@@ -8,4 +8,23 @@ public class ResizableCircle extends Circle implements Resizable {
     public void resize(int percent) {
         radius = radius * (1 + percent/100);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof ResizableCircle)) {
+            return false;
+        }
+        ResizableCircle circle = (ResizableCircle) obj;
+        return radius == circle.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        int code = 17;
+        code = 31 * code + super.hashCode();
+        return code;
+    }
 }
